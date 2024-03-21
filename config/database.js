ec2040e033fc4df634ff1fd5ffb557b9d8a5bd10
srdjanmaljukan <srdjanmaljukan96@gmail.com>
2024-03-21 11:14:24 +0100

    Configure database

--- config/database.js
+++ config/database.js
@@ -51,9 +51,9 @@ module.exports = ({ env }) => {
       connection: {
         connectionString: env('DATABASE_URL'),
         host: env('DATABASE_HOST'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'bge-db'),
-        user: env('DATABASE_USERNAME', 'postgres'),
+        port: env.int('DATABASE_PORT'),
+        database: env('DATABASE_NAME'),
+        user: env('DATABASE_USERNAME'),
         password: env('DATABASE_PASSWORD'),
         ssl: env.bool('DATABASE_SSL', false) && {
           key: env('DATABASE_SSL_KEY', undefined),
