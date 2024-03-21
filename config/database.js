a4d1197a03abc59734fc13f5d97ec084b16b5a51
srdjanmaljukan <srdjanmaljukan96@gmail.com>
2024-03-21 12:10:24 +0100

    Config db

--- config/database.js
+++ config/database.js
@@ -6,11 +6,11 @@ module.exports = ({ env }) => {
       connection: {
         client: "postgres",
         connection: {
-          host: env("PGHOST", "127.0.0.1"),
+          host: env("PGHOST", "bge-db.c7cem0o2iqlt.eu-north-1.rds.amazonaws.com"),
           port: env.int("PGPORT", 5432),
-          database: env("PGDATABASE", "strapi"),
-          user: env("PGUSER", "strapi"),
-          password: env("PGPASSWORD", "strapi"),
+          database: env("PGDATABASE", "bge-db"),
+          user: env("PGUSER", "postgres"),
+          password: env("PGPASSWORD", "Malabajadera3.-!"),
           ssl: env("DATABASE_SSL", false),
         },
         pool: {
